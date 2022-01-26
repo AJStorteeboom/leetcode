d2a2e1bda03db77695b437025bf361f6ffe5fca7
Anne Jan Storteboom <>
2022-01-26 08:36:29 +0100

    2-add-two-numbers

--- 2-add-two-numbers/main.cpp
+++ 2-add-two-numbers/main.cpp
@@ -1,6 +1,72 @@
 #include <iostream>
+#include <charconv>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+
+	ListNode() : val(0), next(nullptr) {}
+
+	ListNode(int x) : val(x), next(nullptr) {}
+
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+	ListNode* dummyHead = new ListNode(0);
+	ListNode* l3 = dummyHead;
+
+	int carry = 0;
+
+	while (l1 != nullptr || l2 != nullptr) {
+		int l1Val = (l1 != nullptr) ? l1->val : 0;
+		int l2Val = (l2 != nullptr) ? l2->val : 0;
+
+		int sum = l1Val+l2Val+carry;
+		carry = sum/10;
+		int lastDigit = sum % 10;
+
+		ListNode* newNode = new ListNode(lastDigit);
+		l3->next = newNode;
+
+		if (l1 != nullptr) l1 = l1->next;
+		if (l2 != nullptr) l2 = l2->next;
+
+		l3 = l3->next;
+	}
+
+	if(carry == 1) {
+		ListNode* newNode = new ListNode(1);
+		l3->next = newNode;
+		l3 = l3->next;
+	}
+
+	return dummyHead->next;
+}
+
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+	ListNode listNode1;
+	ListNode listNode2;
+	ListNode listNode3;
+	listNode1.val = 5;
+	listNode2.val = 0;
+	listNode3.val = 3;
+
+	listNode1.next = &listNode2;
+	listNode2.next = &listNode3;
+
+	ListNode* result = addTwoNumbers(&listNode1, &listNode1);
+
+	while (result != nullptr) {
+		cout << result->val << " - ";
+		result = result->next;
+	}
+
 	return 0;
 }
--- readme.md
+++ readme.md
@@ -12,9 +12,9 @@ This repo contains all my leetcode assignments. You'll find the ones that I'm cu
         <th>Date last checked</th>
     </tr>
     <tr>
-        <td>2</td>
-        <td>Add Two Numbers</td>
-        <td>25-01-2022</td>
+        <td></td>
+        <td></td>
+        <td></td>
     </tr>
 </table>
 
@@ -27,12 +27,21 @@ This repo contains all my leetcode assignments. You'll find the ones that I'm cu
         <th>Id</th>
         <th>Name</th>
         <th>Related topics</th>
+        <th>Cheated</th>
         <th>Date completed</th>
     </tr>
     <tr>
         <td>1</td>
         <td>Two Sum</td>
         <td>Array, Hash Table</td>
+        <td>N</td>
         <td>24-01-2022</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Add Two Numbers</td>
+        <td>Linked List, Math, Recursion</td>
+        <td>Y</td>
+        <td>25-01-2022</td>
     </tr>
 </table>
