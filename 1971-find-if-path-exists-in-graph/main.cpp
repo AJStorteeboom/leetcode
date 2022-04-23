c2c36ee26aa1d15f533d3605f81eb3deed922f72
Anne Jan Storteboom <>
2022-04-23 14:46:17 +0200

    1971: Find if Path Exists in Graph

--- 1971-find-if-path-exists-in-graph/main.cpp
+++ 1971-find-if-path-exists-in-graph/main.cpp
@@ -3,12 +3,16 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <queue>
+#include <stack>
+
 
 using namespace std;
 
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>> &edges, int source, int destination) {
+
+    //BFS
+    bool validPath_1(int n, vector<vector<int>> &edges, int source, int destination) {
         unordered_map<int, vector<int>> graph;
 
         //Add both edges because the graph is bi-directional
@@ -36,9 +40,57 @@ public:
 
         return false;
     }
+
+
+    //DFS
+    bool validPath_2(int n, vector<vector<int>> &edges, int source, int destination) {
+        unordered_map<int, vector<int>> graph;
+
+        //Add both edges because the graph is bi-directional
+        for (vector<int> edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        unordered_set<int> visited;
+        stack<int> stack;
+
+        stack.push(source);
+
+        while (!stack.empty()) {
+            int currentEdge = stack.top();
+            visited.insert(currentEdge);
+            stack.pop();
+
+            if (currentEdge == destination) return true;
+
+            for (int edge : graph[currentEdge]) {
+                if (visited.find(edge) == visited.end()) stack.push(edge);
+            }
+        }
+
+        return false;
+    }
 };
 
+// Test Case
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution sol;
+
+    vector<vector<int>> edges = {
+            {0,7},
+            {0,8},
+            {6,1},
+            {2,0},
+            {0,4},
+            {5,8},
+            {4,7},
+            {1,3},
+            {3,5},
+            {6,5}
+    };
+
+    cout << sol.validPath_2(10, edges, 7, 5) << endl;
+
     return 0;
 }
