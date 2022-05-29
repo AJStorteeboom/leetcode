34a97b2c4ab1db3da8213ed532eb91ba87d56d86
Anne Jan Storteboom <>
2022-05-29 17:18:34 +0200

    1396: Design Underground System

--- 1396-design-underground-system/main.cpp
+++ 1396-design-underground-system/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class UndergroundSystem {
     unordered_map<int,pair<string,int>> customers;
-    unordered_map<string,unordered_map<string,pair<int,int>>> travelData;
+    unordered_map<string,unordered_map<string,pair<int,double>>> travelData;
 
 public:
     UndergroundSystem() {
@@ -31,6 +31,48 @@ public:
 
     double getAverageTime(string startStation, string endStation) {
         return (double) travelData[startStation][endStation].second / travelData[startStation][endStation].first; //Divide total time spend by number of trips
+    }
+};
+
+
+
+class UndergroundSystem_optimized {
+    unordered_map<int,pair<string,int>> customers;
+    unordered_map<string,unordered_map<string,pair<int,int>>> travelData;
+
+public:
+    UndergroundSystem_optimized() {
+
+    }
+
+    void checkIn(int id, string stationName, int t) {
+        if(customers.find(id) != customers.end()) return; //Already checked in
+
+        customers.insert({{id, pair<string,int>(stationName, t)}});
+    }
+
+    void checkOut(int id, string stationName, int t) {
+        if(customers.find(id) == customers.end()) return; //Not checked in;
+
+        int timeSpend = t-customers[id].second;
+
+        //(avg * total trips + timeSpend) / total trips + 1
+        if(travelData[customers[id].first][stationName].first > 0) {
+            travelData[customers[id].first][stationName].second =
+                    (double) travelData[customers[id].first][stationName].second *
+                    travelData[customers[id].first][stationName].first +
+                    timeSpend / travelData[customers[id].first][stationName].first + 1;
+        } else {
+            travelData[customers[id].first][stationName].second = timeSpend;
+        }
+
+        travelData[customers[id].first][stationName].first++; //Increase number of trips
+
+        customers.erase(id);
+    }
+
+    double getAverageTime(string startStation, string endStation) {
+        return (double) travelData[startStation][endStation].second;
     }
 };
 
